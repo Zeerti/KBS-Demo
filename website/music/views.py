d2fccbdff6d95d0e90d5b03e1c15b39339624845
Garrett <skarrj@gmail.com>
2017-03-10 00:00:00 -0700

    Added bootstrap Added ability to add new albums
    Added in bootstrap and basic functionality
    Added in ability to create a new Album.

--- website/music/views.py
+++ website/music/views.py
@@ -1,32 +1,22 @@
-# Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Album
 
-#/music/
-def index(request):
-	all_albums = Album.objects.all()
-	return render(request, 'music/index.html', { 'all_albums' : all_albums } )
 
-#/music/<album_id>/
-#/music/2/
-def detail(request, album_id):
-	album = get_object_or_404(Album, pk=album_id)
-	return render(request, 'music/detail.html', { 'album' : album })
 
-#/music/<album_id>/favorite
-def favorite(request, album_id):
-	album = get_object_or_404(Album, pk=album_id)
+class IndexView(generic.ListView):
+	template_name = 'music/index.html'
+	context_object_name = 'all_albums'
 
-	try:
-		selected_song = album.song_set.get(pk=request.POST['song'])
-	except(KeyError, Song.DoesNotExist):
-		return render(request, 'music/detail.html', {
-			'album':album,
-			'error_message': "You did not select a song",
-			})
-	else:
-		selected_song.is_favorite = True
-		selected_song.save() #MUST BE CALLED TO SAVE TO DB
-		return render(request, 'music/detail.html', { 'album' : album } )
+	def get_queryset(self):
+		return Album.objects.all()
 
-	
+class DetailsView(generic.DetailView):
+	model = Album
+	template_name = 'music/detail.html'
+
+
+class AlbumCreate(CreateView):
+	model = Album
+	fields = ['artist', 'album_title', 'genre', 'album_logo']
+
